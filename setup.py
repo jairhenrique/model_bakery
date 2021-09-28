06f58ef04fa4d62d6ef9e0873cf2ca7e4d05adbc
Rustem Saiargaliev <rustem.saiargaliev@voiio.de>
2021-09-28 21:49:54 +0200

    Add Django 4.0 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
